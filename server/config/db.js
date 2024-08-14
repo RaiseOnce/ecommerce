117fcbea9a5b18ec0502922c2b7c80fd79b93224
Otto Rahn <elias.yunusoff@gmail.com>
2024-08-15 02:00:10 +0500

    servers are update

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect('mongodb://localhost:27017/ecommerce', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: false,
     useCreateIndex: true,
-  });
-  console.log("Database Connected Successfully");
+  })
+  console.log('Database Connected Successfully')
 } catch (err) {
-  console.log("Database Not Connected");
+  console.log('Database Not Connected')
 }
